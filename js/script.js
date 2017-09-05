73e03dafbe6a7ad2c37ead7354093fe6fc15fda2
Alexander Kaykatsishvili <alexander.kaykatsishvili@gmail.com>
2017-09-05 19:50:22 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,69 @@
+;(function($){
+	"use strict";
+
+	$('.ingredients__photo-slider').slick({
+
+});
+	$('.social').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+  	 responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+	$('.main-top-product-slider').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+  	 responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+	$('#table').on("mouseenter", function(){
+		$('.table').addClass('tablevis');
+	});
+	$('#table').on("mouseleave", function(){
+		$('.table').removeClass('tablevis');
+	});
+})(jQuery);
